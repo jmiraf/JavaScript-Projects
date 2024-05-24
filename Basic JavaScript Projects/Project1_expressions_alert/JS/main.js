10a2920ec9cb286f5353e5c7ff7cc63a10c5b135
jmiraf <165960587+jmiraf@users.noreply.github.com>
2024-05-24 13:02:24 -0600

    project 1

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,8 @@
+var x = "Escape Character\' Assignment", y = "String Assignment"; // multiple variables
+document.write(x + "<br> line break <br>");
+
+//window alert
+window.alert("This is a window alert!--- " + y);
+
+// an expression
+document.write(3 + 3);
